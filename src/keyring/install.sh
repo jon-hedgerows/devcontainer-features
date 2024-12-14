89b4572f7dda2107ee7adf420176c888855e0fa1
Jon Davies <jon@hedgerows.org.uk>
2024-12-14 18:46:50 +0000

    depend on python and install with pip

--- src/keyring/install.sh
+++ src/keyring/install.sh
@@ -1,20 +1,11 @@
 #!/bin/sh
 set -e
 
-# the version of pipx that comes with ubuntu is too old for the --global flag
-# so install with pip
-if [ -z $(which pip3) ]; then
-    echo "Installing pip"
-    apt-get -yqq update
-    apt-get -yqq install python3-pip
-fi
-echo "Installing pipx"
-pip3 install --break-system-packages pipx
+# we depend on python, so assume that pip is available
 
 # install keyring and sagecipher
 # sagecipher lets us use an ssh key as a key for the keyring
 
-echo "Installing keyring and sagecipher using pipx"
+echo "Installing keyring and sagecipher using pip"
 
-pipx install --global --include-deps sagecipher  # this also installs keyring
-pipx inject --global sagecipher setuptools       # missing runtime dependency for sagecipher
+pip install --break-system-packages sagecipher
